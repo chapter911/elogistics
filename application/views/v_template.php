e778390526271dc9d1d35a37c30d03d49e0dbbd5
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-01-30 22:10:55 +0700

    update font

--- application/views/v_template.php
+++ application/views/v_template.php
@@ -17,9 +17,9 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
+    <!-- <link
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
-        rel="stylesheet" />
+        rel="stylesheet" /> -->
 
     <link rel="stylesheet" href="<?= base_url(); ?>assets/vendor/fonts/fontawesome.css" />
     <link rel="stylesheet" href="<?= base_url(); ?>assets/vendor/fonts/tabler-icons.css" />
