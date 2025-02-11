fa7158c1a52d544989d4032aeacf393ef8faee18
agung.justik <agung.justik@gmail.com>
2025-02-11 02:58:56 +0000

    add map library

--- application/views/v_template.php
+++ application/views/v_template.php
@@ -69,6 +69,14 @@
 
     <script src="<?= base_url(); ?>assets/vendor/libs/chartjs/chartjs.js"></script>
 
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+
     <script>
     var timeoutInMilliseconds = 1800000;
 
