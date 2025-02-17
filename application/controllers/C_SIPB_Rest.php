e400090a275c87b028f76131cacbaacd2ed2d4f9
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-02-17 23:40:32 +0700

    fix: remove welcome message response from index_get method in C_SIPB_Rest

--- application/controllers/C_SIPB_Rest.php
+++ application/controllers/C_SIPB_Rest.php
@@ -10,11 +10,6 @@ class C_SIPB_Rest extends REST_Controller {
     }
 
     function index_get() {
-        $data = array("status" => "success", "message" => "Welcome to SIPB REST API");
-        $this->response($data, 200);
-    }
-
-    function getKR() {
         $this->db->where('no_spj', $this->get('kr'));
         $data = $this->db->get('trn_sipb_hdr')->result();
         $this->response($data, 200);
