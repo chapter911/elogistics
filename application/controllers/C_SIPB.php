0963bc11704f433052f7290130f190d2bf97727a
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-02-17 20:07:34 +0700

    fix: update URL scheme in test method of C_SIPB controller

--- application/controllers/C_SIPB.php
+++ application/controllers/C_SIPB.php
@@ -56,7 +56,7 @@ class C_SIPB extends CI_Controller
 
     function test()
     {
-        $url = "10.3.0.185:8088/api/v.2.1/check_kr?no_kr=0169.Pj/DAN.01.02/F06050000/2024";
+        $url = "http://10.3.0.185:8088/api/v.2.1/check_kr?no_kr=0169.Pj/DAN.01.02/F06050000/2024";
         $data['url'] = $url;
         $data['data'] = json_decode($this->curl->simple_get($url));
         echo '<pre>'; print_r($data); echo '</pre>';
