ca7c565ec9fd51ce850c7d01aa815757ceba48d9
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-02-17 23:45:35 +0700

    feat: enhance index_get method in C_SIPB_Rest to handle empty 'kr' parameter and return appropriate responses

--- application/controllers/C_SIPB.php
+++ application/controllers/C_SIPB.php
@@ -15,7 +15,6 @@ class C_SIPB extends CI_Controller
     {
         parent::__construct();
 
-
         $this->load->library('Pdf');
         $this->load->library('Curl');
 
--- application/controllers/C_SIPB_Rest.php
+++ application/controllers/C_SIPB_Rest.php
@@ -2,6 +2,13 @@
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
+/**
+ * @property M_AllFunction $M_AllFunction
+ * @property Session $session
+ * @property Template $template
+ * @property Uri $uri
+ */
+
 class C_SIPB_Rest extends REST_Controller {
 
     function __construct($config = 'rest') {
@@ -10,8 +17,24 @@ class C_SIPB_Rest extends REST_Controller {
     }
 
     function index_get() {
-        $this->db->where('no_spj', $this->get('kr'));
-        $data = $this->db->get('trn_sipb_hdr')->result();
-        $this->response($data, 200);
+        if($this->get('kr') == "") {
+            $data['status'] = "failed";
+            $data['message'] = "no kr is not found";
+            $data['data'] = "no kr is not found";
+            $this->response($data, 404);
+        } else {
+            $header = $this->m_allfunction->CustomQuery("SELECT * FROM trn_sipb_hdr WHERE no_spj = '".$this->get('kr')."'");
+            if(count($header) > 0) {
+                $data['status'] = "success";
+                $data['message'] = "data found";
+                $data['data'] = $header;
+                $this->response($data, 200);
+            } else {
+                $data['status'] = "failed";
+                $data['message'] = "data not found";
+                $data['data'] = "data not found";
+                $this->response($data, 404);
+            }
+        }
     }
 }
