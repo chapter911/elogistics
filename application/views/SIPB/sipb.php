20226b8614bc8341ac7fbba55e9934f23797106a
chapter911 <51829448+chapter911@users.noreply.github.com>
2025-03-04 13:11:15 +0700

    fix: hide save button when SIPB detail is visible

--- application/views/SIPB/sipb.php
+++ application/views/SIPB/sipb.php
@@ -618,6 +618,7 @@ function detail(no_sipb, form_name, is_selesai){
                 $('#visible_sipb').show();
             } else {
                 $('#visible_sipb').show();
+                $('#btn_simpan').hide();
                 // $('#visible_sipb').hide();
             }
             $('#createApp2').modal('show');
